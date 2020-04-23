bd063a130757b7337fde4b7fb77055e9bde17846
Eyad Web Developmnet <30596418+development1soft@users.noreply.github.com>
2020-04-24 00:20:08 +0300

    Edited Code

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,7 @@
-import 'package:Dart_Mastering_Level_1/Dart_Mastering_Level_1.dart' as Dart_Mastering_Level_1;
+// import 'package:Dart_Mastering_Level_1/Dart_Mastering_Level_1.dart' as Dart_Mastering_Level_1;
 
-main(List<String> arguments) {
-  print('Hello world: ${Dart_Mastering_Level_1.calculate()}!');
-}
+// main(List<String> arguments) {
+//   print('Hello world: ${Dart_Mastering_Level_1.calculate()}!');
+// }
+
+// edited
