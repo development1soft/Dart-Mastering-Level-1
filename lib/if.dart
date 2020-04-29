65ad1fa61ece0c35397a146fe79e1844e4498645
Eyad Web Developmnet <30596418+development1soft@users.noreply.github.com>
2020-04-29 22:08:55 +0300

    operators and if conditions finished successfully

---
+++ lib/if.dart
@@ -0,0 +1,19 @@
+// x = 5;
+
+// if(x > 6){
+
+//   print('welcome');
+
+// }else if(x == true){
+  
+//   print('yes');
+
+// }else if(x < 0){
+  
+//   print('no');
+
+// }else{
+  
+//   print('sorry');
+
+// }
